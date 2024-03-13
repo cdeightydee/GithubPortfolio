0a43d67d0dced74fb11dbdc9bbacfc05dd5edb7a
cdeightydee <160589800+cdeightydee@users.noreply.github.com>
2024-03-13 00:23:06 -0400

    kk

--- send_email.php
+++ send_email.php
@@ -3,16 +3,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    
+
     $to = "duplecitystudio@gmail.com"; // Your email address
     $subject = "New message from $name";
     $body = "Name: $name\nEmail: $email\nMessage: $message";
     $headers = "From: $email";
-    
+
     if (mail($to, $subject, $body, $headers)) {
         echo "Your message has been sent successfully!";
     } else {
         echo "Oops! Something went wrong. Please try again later.";
     }
 }
-?>
